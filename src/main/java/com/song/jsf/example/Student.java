948217d534b4369420fb7d2f30d9adee36d93c7a
PEDRO ARNAUTH LEITÃO TAVARES <a41490@alunos.isel.pt>
2018-04-09 13:37:21 +0200

    Age added to Student

--- src/main/java/com/song/jsf/example/Student.java
+++ src/main/java/com/song/jsf/example/Student.java
@@ -7,21 +7,24 @@ public class Student implements Serializable {
 
 	private Long id;
 	private String name;
+	private int age;
 
 	public Student() {}
-	public Student(Long id, String name) {
+	public Student(Long id, String name, int age) {
 		this.id = id;
 		this.name = name;
+		this.age=age;
 	}
 
 	public Long getId() { return id; }
 	public void setId(Long id) { this.id = id; }
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
-
+	public int getAge() { return age; }
+	public void setAge(int age) { this.age = age; }
 	@Override
 	public Student clone() {
-		return new Student(id, name);
+		return new Student(id, name, age);
 	}
 
 	public void restore(Student student) {
