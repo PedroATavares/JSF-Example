12387bad0f9659d2c9b39d93c7a8af88295dcea9
PEDRO ARNAUTH LEITÃO TAVARES <a41490@alunos.isel.pt>
2018-04-12 13:51:20 +0200

    Education Level Added

--- src/main/java/com/song/jsf/example/Student.java
+++ src/main/java/com/song/jsf/example/Student.java
@@ -9,20 +9,36 @@ public class Student implements Serializable {
 	private Long id;
 	private String name;
 	private int age;
+	private String educationLevel;
 	
 	public Student() {}
 	public Student(Long id, String name, int age) {
 		this.id = id;
 		this.name = name;
 		this.age=age;
+		setEducationLevel(age);
 	}
 
+	public void setEducationLevel(int age) throws IllegalArgumentException {
+		if(age>12 && age<=16) {educationLevel="Elementary"; return;}
+		if(age>16 && age<=22) {educationLevel="Secondary"; return;}
+		if(age>22) {educationLevel="Higher"; return;}
+		throw new IllegalArgumentException("Age must be higher or equal to 13");
+		
+	}
+	
+	public String getEducationLevel() { return educationLevel; }
+
+	
 	public Long getId() { return id; }
 	public void setId(Long id) { this.id = id; }
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
 	public int getAge() { return age; }
-	public void setAge(int age) { this.age = age; }
+	public void setAge(int age) { 
+		this.age = age;
+		setEducationLevel(age);
+	}
 	@Override
 	public Student clone() {
 		return new Student(id, name, age);
